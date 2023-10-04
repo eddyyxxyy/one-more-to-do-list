78339e010058701cb2ba63ed0bde9453f3ab5bc3
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-10-04 12:20:06 -0300

    feat: implements ButtonPlus and adds extra attr to form and Input

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,29 +1,48 @@
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 import { Header } from './components/Header';
 import { Input } from './components/Input';
+import { ButtonPlus } from './components/ButtonPlus';
 
 import styles from './App.module.css';
 
 export function App() {
   const [newTask, setNewTask] = useState('');
 
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
+  function handleCreateTask(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log('Create task');
+  }
+
   return (
     <div className={styles.app}>
       <Header />
       <main className={styles.main}>
-        <form className={styles.newTask}>
+        <form className={styles.newTask} onSubmit={handleCreateTask}>
           <label className="srOnly" htmlFor="newTask">
             Adicione uma nova tarefa
           </label>
           <Input
             type="text"
+            id="newTask"
             name="newTask"
             value={newTask}
+            onChange={handleInputChange}
             aria-label="Adicione uma nova tarefa"
             placeholder="Adicione uma nova tarefa"
-            onChange={(e) => setNewTask(e.target.value)}
             required
+          />
+          <ButtonPlus
+            type="submit"
+            text="Criar"
+            title="Criar tarefa"
+            aria-label="Criar tarefa"
+            disabled={!newTask}
           />
         </form>
       </main>
