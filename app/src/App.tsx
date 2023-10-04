658170ea53803c5ab6f5d1c8bbb345850b11bcf5
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-10-04 14:22:20 -0300

    feat: checks for tasks in localStorage, adds task counters and no tasks registered yet view

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,42 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 
 import { Header } from './components/Header';
 import { Input } from './components/Input';
 import { Button } from './components/Button';
 
 import styles from './App.module.css';
-import { PlusCircle } from '@phosphor-icons/react';
+import { ClipboardText, PlusCircle } from '@phosphor-icons/react';
 
 export function App() {
-  const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState<object[]>([]);
+  const [createdTasks, setCreatedTasks] = useState(0);
+  // const [finishedTasks, setFinishedTasks] = useState(0); -> Contains the sum of all finished tasks
+  const [newTaskDescription, setNewTaskDescription] = useState('');
 
   function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
-    setNewTask(event.target.value);
+    setNewTaskDescription(event.target.value);
   }
 
   function handleCreateTask(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    console.log('Create task');
+    setCreatedTasks((prevState) => prevState + 1);
   }
+
+  useEffect(() => {
+    const savedTasks = localStorage.getItem('@todo-list-tasks:') ?? 0;
+
+    if (savedTasks) {
+      const tasksToBeInserted = JSON.parse(savedTasks) as object[];
+
+      setCreatedTasks(tasksToBeInserted.length);
+      setTasks([...tasksToBeInserted]);
+
+      return;
+    }
+
+    setTasks([]);
+  }, [createdTasks]);
 
   return (
     <div className={styles.app}>
@@ -32,7 +50,7 @@ export function App() {
             type="text"
             id="newTask"
             name="newTask"
-            value={newTask}
+            value={newTaskDescription}
             onChange={handleInputChange}
             aria-label="Adicione uma nova tarefa"
             placeholder="Adicione uma nova tarefa"
@@ -46,9 +64,35 @@ export function App() {
             iconSize={18}
             iconWeight="regular"
             aria-label="Criar tarefa"
-            disabled={!newTask}
+            disabled={!newTaskDescription}
           />
         </form>
+        <section className={styles.tasks}>
+          <header className={styles.tasksHeader}>
+            <h2 className="srOnly">Tarefas</h2>
+            <h4>
+              Tarefas criadas <span>{createdTasks}</span>
+            </h4>
+            <h4>
+              Concluídas <span>0</span>
+            </h4>
+          </header>
+          {!tasks.length ? (
+            <div className={styles.noTasksCreated}>
+              <ClipboardText
+                size={56}
+                weight="regular"
+                color="var(--gray-400)"
+              />
+              <div>
+                <h3>Você ainda não tem tarefas cadastradas</h3>
+                <p>Crie tarefas e organize seus itens a fazer</p>
+              </div>
+            </div>
+          ) : (
+            <ul className={styles.tasksList}></ul>
+          )}
+        </section>
       </main>
     </div>
   );
